773e1ac82cbef516d0d5b57067cddd66158b386b
istrauch2023 <istrauch2023@gmail.com>
2024-12-26 09:29:22 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1735186663,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ПлиточныйФон.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт5-sheet0.png",
+		"images/Спрайт6-sheet0.png",
+		"images/Спрайт7-sheet0.png",
+		"images/Спрайт8-sheet0.png",
+		"images/Спрайт9-sheet0.png",
+		"images/Спрайт10-sheet0.png",
+		"images/Спрайт11-sheet0.png",
+		"images/Спрайт12-sheet0.png",
+		"images/Спрайт13-sheet0.png",
+		"images/Спрайт16-sheet0.png",
+		"images/Спрайт17-sheet0.png",
+		"images/ПлиточныйФон2.png",
+		"images/Спрайт18-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
